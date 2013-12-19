f578811c9a784ed0fb78759b7e0f115dc6a3ebc4
Stella <stellatsecyin@gmail.com>
2013-12-19 20:37:08 +0800

    update edit

--- static/js/app/route.js
+++ static/js/app/route.js
@@ -2,6 +2,7 @@
 $('#add_to').click(function(){
     var my = $('#my_route');
     var current = $('#current_route').html();
+    var markerArr = [];
     my.append(current);
     $.ajax({
         url: "/get_sights",
@@ -10,7 +11,7 @@ $('#add_to').click(function(){
     }).done(function (data){
         obj = JSON.parse(data)
         for(var i=0;i<obj.length;i++){
-            pots.push({title:obj[i].name,content:obj[i].id,point:obj[i].latitude + '|' + obj[i].longitude,isOpen:0,icon:{w:21,h:21,l:0,t:0,x:6,lb:5}})
+            markerArr.push({title:obj[i].name,content:obj[i].id,point:obj[i].latitude + '|' + obj[i].longitude,isOpen:0,icon:{w:21,h:21,l:0,t:0,x:6,lb:5}})
             
           }
         reload_map();

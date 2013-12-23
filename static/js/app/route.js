489b414e3f1c1e1a0a795f1d28ab1a8a13f102fe
Stella <stellatsecyin@gmail.com>
2013-12-23 16:29:50 +0800

    update route

--- static/js/app/route.js
+++ static/js/app/route.js
@@ -18,9 +18,33 @@ $('#add_to').click(function(){
     })
     
 })
-function reload_route(route, route_name){
-    $('.route_content').html(route);
-    $('.route_name').html(route_name)
+$if ret['current'] == '1':
+    <li class="previous disabled"><a href="#" >&larr; 上一个</a>
+$else:
+    <li class="previous"><a href="javascript:void(0)" onclick="find_route($ret['prev'])">&larr; 上一个</a>
+</li>
+$if int(ret['current']) == ret['count']:
+    <li class="next disabled"><a href="#">下一个 &rarr;</a>
+$else:
+    <li class="next"><a href="javascript:void(0)" onclick="find_route($ret['next'])">下一个 &rarr;</a>
+</li>
+function reload_route(sights, route){
+    $('.route_content').html(sights);
+    $('.route_name').html(route['name']);
+    var pager = '';
+    if (route['current'] == '1'){
+        pager += '<li class="previous disabled"><a href="#" >&larr; 上一个</a>'
+    } else {
+        pager += '<li class="previous"><a href="javascript:void(0)" onclick="find_route(' + route['prev'] +')">&larr; 上一个</a>'
+    }
+    pager += '</li>'
+    if (route['current'] == route['count']){
+        pager += '<li class="next disabled"><a href="#">下一个 &rarr;</a>'
+    } else {
+        pager += '<li class="next"><a href="javascript:void(0)" onclick="find_route(' + route['next'] + ')">下一个 &rarr;</a>';
+    }
+    pager += '</li>'
+    $('#route_pager').html(pager)
 };
 function find_route(route_id){
     var sight = '';
@@ -32,7 +56,7 @@ function find_route(route_id){
         for(var i=0;i<obj.length;i++){
             sight += '<tr><td><img width="90" height="60" style="margin:0px 0px 0px -15px" src="'+obj[i]['sight'][1]+'"></td><td><a>'+obj[i]['sight'][0]+'</a><br/><p style="font-size:11px">建议游玩：'+obj[i]['sight'][2]+'</i></td></tr>'
           }
-        reload_route(sight,ret['ret']['route_name']);
+        reload_route(sight,ret['ret']);
     })
 }
 
